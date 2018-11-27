bugfix for mean calculation

trajectory.py
@@ -21,7 +21,7 @@ state_size_mode2 = 2
 # model parameters
 c = 4.0
 k_on = 5.0
-k_off = 1.0
+k_off = 40.0
 k_p = 2.0
 
 # model identities
@@ -111,15 +111,15 @@ def multitraj(num_traj, num_steps=NUM_STEPS, bound_fraction=0.0):
 
 def get_state_at_t(traj, times, t):
     step = 0
-    while times[step] < t:
+    while times[step] <= t:
         step += 1
-    return traj[step, :]
+    return traj[step - 1, :]
 
 
 def get_mean_var_timeseries(traj_array, times_array):
     num_traj = np.shape(traj_array)[-1]
-    dt = np.mean(times_array[1,:])
-    endtime = np.min(times_array[-1,:])
+    dt = np.mean(times_array[1, :])
+    endtime = np.min(times_array[-1, :])
     moment_times = np.arange(0.0, endtime, dt)
     mean_vals = np.zeros(len(moment_times))
     var_vals = np.zeros(len(moment_times))
@@ -133,7 +133,7 @@ def get_mean_var_timeseries(traj_array, times_array):
             statesquaresum += state_at_t[1]**2
 
         mean_vals[idx] = statesum / num_traj
-        var_vals[idx] = statesquaresum/num_traj -  mean_vals[idx]**2
+        var_vals[idx] = statesquaresum / num_traj - mean_vals[idx]**2
     return mean_vals, var_vals, moment_times
 
 
@@ -180,9 +180,9 @@ def theory_cumulants(moment_times, label, init_n=0.0, init_p1=0.0):
 
 if __name__ == '__main__':
     # settings
-    num_traj = 200
-    num_steps = 300
-    init_bound = pss
+    num_traj = 1000
+    num_steps = 100
+    init_bound = 1.0
 
     # compute
     traj_array, times_array = multitraj(num_traj, bound_fraction=init_bound, num_steps=num_steps)
@@ -200,9 +200,11 @@ if __name__ == '__main__':
     plt.plot(moment_times, mean_vals + np.sqrt(var_vals), '--k', lw=2)
     plt.plot(moment_times, mean_vals - np.sqrt(var_vals), '--k', lw=2)
     # plot theory cumulants (direct)
+    """
     plt.plot(moment_times, mean_vals_direct, 'b', lw=2, label="direct")
     plt.plot(moment_times, mean_vals_direct + np.sqrt(var_vals_direct), '--b', lw=2)
     plt.plot(moment_times, mean_vals_direct - np.sqrt(var_vals_direct), '--b', lw=2)
+    """
     # plot theory cumulants (generating function)
     plt.plot(moment_times, mean_vals_gen, 'r', lw=2, label="generating")
     plt.plot(moment_times, mean_vals_gen + np.sqrt(var_vals_gen), '--r', lw=2)
@@ -211,6 +213,17 @@ if __name__ == '__main__':
     plt.title('Mode 1 <n>(t) +- sqrt(var(t)) for %d trajectories' % num_traj)
     plt.xlabel('time')
     plt.ylabel('n')
+    plt.legend()
+    plt.show()
+
+    # only means
+    plt.figure()
+    plt.scatter(moment_times, mean_vals, s=4.0, c='k', marker='s', label="data", alpha=0.5)
+    plt.scatter(moment_times, mean_vals_direct, s=4.0, c='b', marker='s', label="direct", alpha=0.5)
+    plt.scatter(moment_times, mean_vals_gen, s=4.0, c='r', marker='s', label="generating", alpha=0.5)
+    plt.title('Mode 1 mean(n) for %d trajectories' % num_traj)
+    plt.xlabel('time')
+    plt.ylabel('<n>(t)')
     plt.legend()
     plt.show()
 