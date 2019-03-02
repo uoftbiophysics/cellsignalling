c9143811787aa14386eb5a8f3ac26059e6db528b
Matthew Smart <mattsmart@hotmail.ca>
2019-03-02 11:34:39 -0500

    init

---
+++ figures/heatmaps.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import os
+
+from load_inputs import DATADICT
+from settings import DIR_OUTPUT
+from settings import COLOR_SCHEME as cs
+
+plt.style.use('parameters.mplstyle')  # particularIMporting
+
+
+def heatmap_mode1_error_c():
+    return
+
+
+def heatmap_mode1_error_koff():
+    return
+
+
+def heatmap_combined_error_c():
+    return
+
+
+def heatmap_combined_error_koff():
+    return
+
+
+if __name__ == '__main__':
+    heatmap_mode1_error_c()
+    heatmap_mode1_error_koff()
+    heatmap_combined_error_c()
+    heatmap_combined_error_koff()
