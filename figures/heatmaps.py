e741dec770456dae493bae9b6315a055b0aabffd
Duncan Kirby <39995281+dakirby@users.noreply.github.com>
2019-08-14 18:00:34 -0400

    Removed dedimensionalised heatmap script and added switches to heatmaps.py

--- figures/heatmaps.py
+++ figures/heatmaps.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-from load_inputs import DATADICT
+#from load_inputs import DATADICT
 from settings import DIR_OUTPUT, DIR_INPUT
 from settings import COLOR_SCHEME as cs
 
 plt.style.use('parameters.mplstyle')  # particularIMporting
 
-# notebook params
+# unit params
 KON = 1
 KP = 10
-T = 100
+T = 1000
 KF = 1.0
+
+# de-dimensionalise params
+c0 = KP/KON
+alpha = KP*T
 
 # plot params
 FS = 16
@@ -29,6 +33,9 @@ LOG_START_KOFF = -3
 LOG_END_KOFF = 3
 TOTAL_POINTS_KOFF = (LOG_END_KOFF - LOG_START_KOFF) * POINTS_BETWEEN_TICKS + 1
 KOFFRANGE = np.logspace(LOG_START_KOFF, LOG_END_KOFF, TOTAL_POINTS_KOFF)
+
+CTILDERANGE = np.divide(list(CRANGE), c0)
+ZRANGE = np.divide(list(KOFFRANGE), KP)
 
 # global vmax and vmin
 assert LOG_START_KOFF == -3
@@ -66,8 +73,8 @@ def plot_heatmap(arr, crange, koffrange, fname, label, show=SHOW):
     ax.set_yticklabels([r'$10^{%d}$' % np.log10(kval) for i, kval in enumerate(koffrange) if i % POINTS_BETWEEN_TICKS==0],
                        fontsize=FS)
     ax.invert_yaxis()
-    ax.set_xlabel(r'$c$', fontsize=FS)
-    ax.set_ylabel(r'$k_{off}$', fontsize=FS)
+    ax.set_xlabel(r'$\tilde{c}$', fontsize=FS)
+    ax.set_ylabel('z', fontsize=FS)
 
     # create colorbar
     cbar = fig.colorbar(im)
@@ -79,7 +86,7 @@ def plot_heatmap(arr, crange, koffrange, fname, label, show=SHOW):
     #for t in cbar.ax.get_yticklabels(): print(t.get_text())
 
     # contour line for value 1.0
-    plt.contour(arr, levels=[1.0], linestyles=['dashed'])  # use 'dashed' or 'solid' curve
+    plt.contour(arr, levels=[10, 100, 1000, 1E4], linestyles=['dashed'])  # use 'dashed' or 'solid' curve
 
     # save
     plt.savefig(DIR_OUTPUT + os.sep + fname + '.pdf')
@@ -90,30 +97,35 @@ def plot_heatmap(arr, crange, koffrange, fname, label, show=SHOW):
     return
 
 
-def heatmap_mode1_error_x(crange=CRANGE, koffrange=KOFFRANGE, make_heatmap=True, make_panel=False):
+def heatmap_mode1_error_x(crange=CTILDERANGE, koffrange=ZRANGE, make_heatmap=True, make_panel=False,
+                          scale_factor=alpha, label_style=0):
     if make_heatmap == True:
-        def mode1_error_x(c, koff):
+        def mode1_error_x(ctilde, z, scale_factor=scale_factor):
+            c = ctilde * c0
+            koff = z * KP
             x = c * KON / koff
             val = (1 + x) / (KP * T * x) * ((1 + x) ** 2 + 2 * KP / koff)
-            return val
+            return scale_factor * val
 
         arr = np.zeros((len(koffrange), len(crange)))
         for i, koffval in enumerate(koffrange):
             for j, cval in enumerate(crange):
                 arr[i, j] = mode1_error_x(cval, koffval)
 
-        label = r'$\langle\delta x^{2}\rangle$/$x^{2}$'
+        label = r'$\alpha \langle\delta x^{2}\rangle$/$x^{2}$'
         plot_heatmap(arr, crange, koffrange, 'heatmap_mode1_heuristic_error_x', label)
 
     if make_panel == True:
-        def cross_section_mode1_error_c(crange=CRANGE):
-            def mode1_error_c(c, koff):
+        def cross_section_mode1_error_c(crange=CTILDERANGE):
+            def mode1_error_c(ctilde, z, scale_factor=scale_factor):
+                c = ctilde * c0
+                koff = z * KP
                 x = c * KON / koff
                 val = (1 + x) / (KP * T * x) * ((1 + x) ** 2 + 2 * KP / koff)
-                return val
+                return scale_factor * val
 
-            arr = [mode1_error_c(cval, 1) for cval in CRANGE]
-            return dict({'xpts': CRANGE, 'ypts':arr})
+            arr = [mode1_error_c(cval, 1) for cval in crange]
+            return dict({'xpts': crange, 'ypts':arr})
 
         figname = 'mode1_error_c_cross_section'
         curve1 = cross_section_mode1_error_c()
@@ -122,12 +134,18 @@ def heatmap_mode1_error_x(crange=CRANGE, koffrange=KOFFRANGE, make_heatmap=True,
         #plt.figure()
         plt.plot(curve1['xpts'], curve1['ypts'], color=cs['simple_fisher'], label='Simple Fisher', zorder=1)
         # axis
-        plt.title('Mode 1: MLE relative error comparison \n($k_p=10$, $t=100$, $k_{off}=k_{on}=1$)')
-        plt.xlabel(r'$c$')
-        plt.ylabel(r'$\langle\delta c^{2}\rangle$/$c^{2}$')
+        if label_style == 0:
+            plt.title('Mode 1: MLE relative error comparison \n'+r'($\tilde{c}_0=10$, $\alpha=1 \times 10^4$, $k_{p}=10$)')
+            plt.ylabel(r'$\alpha \langle\delta c^{2}\rangle$/$c^{2}$')
+        elif label_style == 1:
+            plt.title('Mode 1: MLE relative error comparison \n($k_p=10$, $t=100$, $k_{off}=k_{on}=1$)')
+            plt.ylabel(r'$\langle\delta c^{2}\rangle$/$c^{2}$')
+
+        plt.xlabel(r'$\tilde{c}_0$')
+
         plt.gca().set_xscale('log')
-        plt.xlim([1E-3, 1E3])
-        plt.ylim([0, 1])
+        plt.xlim([CTILDERANGE[0], CTILDERANGE[-1]])
+        plt.ylim([0, alpha])
         plt.xscale('log')
         #plt.legend()
         # save figure
@@ -137,67 +155,83 @@ def heatmap_mode1_error_x(crange=CRANGE, koffrange=KOFFRANGE, make_heatmap=True,
     return
 
 
-def heatmap_combined_error_c(crange=CRANGE, koffrange=KOFFRANGE):
-
-    def combined_error_c(c, koff):
-        x = c * KON / koff
+def heatmap_combined_error_c(crange=CTILDERANGE, koffrange=ZRANGE,
+                             scale_factor=alpha, label_style=0):
+    def combined_error_c(ctilde, z, scale_factor=scale_factor):
+        c = ctilde * c0
+        koff = z * KP
+        x = KON * c / koff
         num = 2 * KP * x + koff * KP * T * (1 + x) ** 3 + koff**2 * T * x ** 2 * (1 + x) ** 3
         den = koff ** 2 * KP * T**2 * x * (1 + x) ** 2
         val = num / den
-        return val
+        return scale_factor * val
 
     arr = np.zeros((len(koffrange), len(crange)))
     for i, koffval in enumerate(koffrange):
         for j, cval in enumerate(crange):
             arr[i, j] = combined_error_c(cval, koffval)
 
-    label = r'$\langle\delta c^{2}\rangle$/$c^{2}$'
+    if label_style == 0:
+        label = r'$\alpha \langle\delta c^{2}\rangle$/$c^{2}$'
+    elif label_style == 1:
+        label = r'$\langle\delta c^{2}\rangle$/$c^{2}$'
     plot_heatmap(arr, crange, koffrange, 'heatmap_combined_heuristic_error_c', label)
     return
 
 
-def heatmap_combined_error_koff(crange=CRANGE, koffrange=KOFFRANGE):
+def heatmap_combined_error_koff(crange=CTILDERANGE, koffrange=ZRANGE,
+                             scale_factor=alpha, label_style=0):
 
-    def combined_error_koff(c, koff):
+    def combined_error_koff(ctilde, z, scale_factor=scale_factor):
+        c = ctilde * c0
+        koff = z * KP
         x = c * KON / koff
         num = 2 * KP * x + koff * KP * T * (1 + x) ** 3 + koff**2 * T * (1 + x) ** 3
         den = koff ** 2 * KP * T**2 * x * (1 + x) ** 2
         val = num / den
-        return val
+        return scale_factor * val
 
     arr = np.zeros((len(koffrange), len(crange)))
     for i, koffval in enumerate(koffrange):
         for j, cval in enumerate(crange):
             arr[i, j] = combined_error_koff(cval, koffval)
 
-    label = r'$\langle\delta k_{off}^{2}\rangle$/$k_{off}^{2}$'
+    if label_style == 0:
+        label = r'$\alpha \langle\delta k_{off}^{2}\rangle$/$k_{off}^{2}$'
+    elif label_style == 1:
+        label = r'$\langle\delta k_{off}^{2}\rangle$/$k_{off}^{2}$'
     plot_heatmap(arr, crange, koffrange, 'heatmap_combined_heuristic_error_koff', label)
     return
 
-def figure_2_combined_cross_sections(crange=CRANGE, koffrange=KOFFRANGE):
+def figure_2_combined_cross_sections(crange=CRANGE, koffrange=KOFFRANGE,
+                             scale_factor=alpha, label_style=0):
     """
     Produces cross sections of the c and koff relative error heatmaps.
     Both cross sections are for constant koff = 1 while varying c (ie. horizontal cross sections)
     """
-    def combined_error_c(c, koff):
+    def combined_error_c(ctilde, z, scale_factor=scale_factor):
+        c = ctilde * c0
+        koff = z * KP
         x = c * KON / koff
         num = 2 * KP * x + koff * KP * T * (1 + x) ** 3 + koff ** 2 * T * x ** 2 * (1 + x) ** 3
         den = koff ** 2 * KP * T ** 2 * x * (1 + x) ** 2
         val = num / den
-        return val
-    def combined_error_koff(c, koff):
+        return scale_factor * val
+    def combined_error_koff(ctilde, z, scale_factor=scale_factor):
+        c = ctilde * c0
+        koff = z * KP
         x = c * KON / koff
         num = 2 * KP * x + koff * KP * T * (1 + x) ** 3 + koff**2 * T * (1 + x) ** 3
         den = koff ** 2 * KP * T**2 * x * (1 + x) ** 2
         val = num / den
-        return val
+        return scale_factor * val
 
     def cross_section_combined_error_c():
-        arr = [combined_error_c(cval, 1) for cval in CRANGE]
-        return dict({'xpts': CRANGE, 'ypts':arr})
+        arr = [combined_error_c(cval, 1) for cval in crange]
+        return dict({'xpts': crange, 'ypts':arr})
     def cross_section_combined_error_koff():
-        arr = [combined_error_koff(c, 1) for c in CRANGE]
-        return dict({'xpts': KOFFRANGE, 'ypts':arr})
+        arr = [combined_error_koff(c, 1) for c in crange]
+        return dict({'xpts': koffrange, 'ypts':arr})
 
     figname = 'combined_error_cross_sections'
     curve1 = cross_section_combined_error_c()
@@ -207,19 +241,26 @@ def figure_2_combined_cross_sections(crange=CRANGE, koffrange=KOFFRANGE):
     ax1 = plt.gca()
     ax2 = ax1.twiny()
 
-    ln1 = ax1.plot(curve1['xpts'], curve1['ypts'], color=cs['simple_fisher'], label=r'$\delta c^{2}/c^{2}$', zorder=1)
-    ln2 = ax2.plot(curve2['xpts'], curve2['ypts'], color=cs['heuristic'], label=r'$\delta k_{off}^{2}/k_{off}^{2}$', zorder=1)
+    if label_style == 0:
+        ln1 = ax1.plot(curve1['xpts'], curve1['ypts'], color=cs['simple_fisher'], label=r'$\alpha \delta c^{2}/c^{2}$', zorder=1)
+        ln2 = ax2.plot(curve2['xpts'], curve2['ypts'], color=cs['heuristic'], label=r'$\alpha \delta k_{off}^{2}/k_{off}^{2}$', zorder=1)
+        plt.title('Mode 2: MLE relative error comparison\n' + r'($\tilde{c}_0=10$, $\alpha=1 \times 10^4$, $k_{p}=10$)')
+        plt.ylabel(r'$\alpha \langle\delta (\cdot)^{2}\rangle$/$(\cdot)^{2}$')
+    elif label_style == 1:
+        ln1 = ax1.plot(curve1['xpts'], curve1['ypts'], color=cs['simple_fisher'], label=r'$\delta c^{2}/c^{2}$', zorder=1)
+        ln2 = ax2.plot(curve2['xpts'], curve2['ypts'], color=cs['heuristic'],label=r'$\delta k_{off}^{2}/k_{off}^{2}$', zorder=1)
+        plt.title('Mode 2: MLE relative error comparison\n' + r'($k_p=10$, $t=100$, $k_{off}=k_{on}=1$)')
+        plt.ylabel(r'$\langle\delta (\cdot)^{2}\rangle$/$(\cdot)^{2}$')
 
     # axis
-    plt.title('Mode 2: MLE relative error comparison\n($k_p=10$, $t=100$, $k_{on}=k_{off}=1$)')
     ax1.set_xlabel(r'$c$')
     #ax2.set_xlabel(r'$k_{off}$')
-    plt.ylabel(r'$\langle\delta (\cdot)^{2}\rangle$/$(\cdot)^{2}$')
+
     ax1.set_xscale('log')
     ax2.set_xscale('log')
-    ax1.set_xlim([1E-3, 1E3])
-    ax2.set_xlim([1E-3, 1E3])
-    plt.ylim([0, 1])
+    #ax1.set_xlim([1E-3, 1E3])
+    #ax2.set_xlim([1E-3, 1E3])
+    plt.ylim([0, 0.1*alpha])
 
     lns = ln1 + ln2
     labs = [l.get_label() for l in lns]
@@ -231,9 +272,12 @@ def figure_2_combined_cross_sections(crange=CRANGE, koffrange=KOFFRANGE):
     plt.savefig(DIR_OUTPUT + os.sep + figname + '.eps')
 
 
-def heatmap_kpr_error_c(crange=CRANGE, koffrange=KOFFRANGE):
+def heatmap_kpr_error_c(crange=CTILDERANGE, koffrange=ZRANGE,
+                        scale_factor=alpha, label_style=0):
 
-    def kpr_error_c_full(c, koff):
+    def kpr_error_c_full(ctilde, z, scale_factor=scale_factor):
+        c = ctilde * c0
+        koff = z * KP
         x = c * KON / koff
         g = koff / KF
         factor_A = (1 + x) / ((1 + g) * koff**2 * KP * T**2 * x)
@@ -241,48 +285,60 @@ def heatmap_kpr_error_c(crange=CRANGE, koffrange=KOFFRANGE):
         term2 = koff * T * (KP + koff * x**2)
         term3 = g**2 * (koff**2 * T + KP * (2 * (1 + x) + koff * T * (2 + x * (2 + x))))
         val = factor_A * (term1 + term2 + term3)
-        return val
+        return scale_factor * val
 
-    def kpr_error_c_high_g_t(c, koff):
+    def kpr_error_c_high_g_t(ctilde, z, scale_factor=scale_factor):
+        c = ctilde * c0
+        koff = z * KP
         x = c * KON / koff
         left = (1 + koff / KF) * (1 + x) / (koff * T * x)
         right = koff / KP + 2 + 2 * x + x ** 2
         val = left * right
-        return val
+        return scale_factor * val
 
     arr = np.zeros((len(koffrange), len(crange)))
     for i, koffval in enumerate(koffrange):
         for j, cval in enumerate(crange):
             arr[i, j] = kpr_error_c_full(cval, koffval)
-
-    label = r'$\langle\delta c^{2}\rangle$/$c^{2}$'
+    if label_style == 0:
+        label = r'$\alpha \langle\delta c^{2}\rangle$/$c^{2}$'
+    elif label_style == 1:
+        label = r'$\langle\delta c^{2}\rangle$/$c^{2}$'
     plot_heatmap(arr, crange, koffrange, 'heatmap_kpr_heuristic_error_c', label)
     return
 
 
-def heatmap_kpr_error_koff(crange=CRANGE, koffrange=KOFFRANGE):
+def heatmap_kpr_error_koff(crange=CTILDERANGE, koffrange=ZRANGE,
+                        scale_factor=alpha, label_style=0):
 
-    def kpr_error_koff(c, koff):
+    def kpr_error_koff(ctilde, z, scale_factor=scale_factor):
+        c = ctilde * c0
+        koff = z * KP
         # Note the "full" heuristic expression for koff error is the same as the high g high t one
         x = c * KON / koff
         left = (1 + koff / KF) * (1 + x) / (koff * T * x)
         right = koff / KP + 1
         val = left * right
-        return val
+        return scale_factor * val
 
     arr = np.zeros((len(koffrange), len(crange)))
     for i, koffval in enumerate(koffrange):
         for j, cval in enumerate(crange):
             arr[i, j] = kpr_error_koff(cval, koffval)
-
-    label = r'$\langle\delta k_{off}^{2}\rangle$/$k_{off}^{2}$'
+    if label_style == 0:
+        label = r'$\alpha \langle\delta k_{off}^{2}\rangle$/$k_{off}^{2}$'
+    elif label_style == 1:
+        label = r'$\langle\delta k_{off}^{2}\rangle$/$k_{off}^{2}$'
     plot_heatmap(arr, crange, koffrange, 'heatmap_kpr_heuristic_error_koff', label)
     return
 
 
-def heatmap_kpr2_error_c(crange=CRANGE, koffrange=KOFFRANGE):
+def heatmap_kpr2_error_c(crange=CTILDERANGE, koffrange=ZRANGE,
+                        scale_factor=alpha, label_style=0):
 
-    def kpr2_error_c_full(c, koff):
+    def kpr2_error_c_full(ctilde, z, scale_factor=scale_factor):
+        c = ctilde * c0
+        koff = z * KP
         kon = KON
         kf = KF
         kp = KP
@@ -314,21 +370,26 @@ def heatmap_kpr2_error_c(crange=CRANGE, koffrange=KOFFRANGE):
                                                                                                                                                                                              koff**2*(2 - 3*x - 5*x**2 - 2*x**3 + koff*t*(-1 + 2*x + 5*x**2 + 2*x**3)))) /
                                                 ((kf + koff)**4*(kf + koff + kf*x + koff*x)**2))) / \
               (c**2*kf**2*koff*kon**2*kp**3*t**4*x**3*(1 + x)**4*(-(kp*t) + (kf*kp*t*x)/(kf + koff + kf*x + koff*x) + (koff*kp*t*x)/(kf + koff + kf*x + koff*x))**4)
-        return val
+        return scale_factor * val
 
     arr = np.zeros((len(koffrange), len(crange)))
     for i, koffval in enumerate(koffrange):
         for j, cval in enumerate(crange):
             arr[i, j] = kpr2_error_c_full(cval, koffval)
-
-    label = r'$\langle\delta c^{2}\rangle$/$c^{2}$'
+    if label_style == 0:
+        label = r'$\alpha \langle\delta c^{2}\rangle$/$c^{2}$'
+    elif label_style == 1:
+        label = r'$\langle\delta c^{2}\rangle$/$c^{2}$'
     plot_heatmap(arr, crange, koffrange, 'heatmap_kpr2_heuristic_error_c', label)
     return
 
 
-def heatmap_kpr2_error_koff(crange=CRANGE, koffrange=KOFFRANGE):
+def heatmap_kpr2_error_koff(crange=CTILDERANGE, koffrange=ZRANGE,
+                        scale_factor=alpha, label_style=0):
 
-    def kpr2_error_koff(c, koff):
+    def kpr2_error_koff(ctilde, z, scale_factor=scale_factor):
+        c = ctilde * c0
+        koff = z * KP
         # Note the "full" heuristic expression for koff error is the same as the high g high t one
         kon = KON
         kf = KF
@@ -349,27 +410,30 @@ def heatmap_kpr2_error_koff(crange=CRANGE, koffrange=KOFFRANGE):
                                                  ((1 + g)**3*(kf + koff + kf*x + koff*x)**2))) / \
               (kf**2*koff**3*kp**3*t**4*x**3*(1 + x)**4)
 
-        return val
+        return scale_factor * val
 
     arr = np.zeros((len(koffrange), len(crange)))
     for i, koffval in enumerate(koffrange):
         for j, cval in enumerate(crange):
             arr[i, j] = kpr2_error_koff(cval, koffval)
-
-    label = r'$\langle\delta k_{off}^{2}\rangle$/$k_{off}^{2}$'
+    if label_style == 0:
+        label = r'$\alpha \langle\delta k_{off}^{2}\rangle$/$k_{off}^{2}$'
+    elif label_style == 1:
+        label = r'$\langle\delta k_{off}^{2}\rangle$/$k_{off}^{2}$'
     plot_heatmap(arr, crange, koffrange, 'heatmap_kpr2_heuristic_error_koff', label)
     return
 
 
 if __name__ == '__main__':
 
-    #heatmap_mode1_error_x(make_heatmap=False, make_panel=True)
+    heatmap_mode1_error_x(make_heatmap=False, make_panel=True)
+    heatmap_mode1_error_x()
     figure_2_combined_cross_sections()
-    """
+
     heatmap_combined_error_c()
     heatmap_combined_error_koff()
     heatmap_kpr_error_c()
     heatmap_kpr_error_koff()
-    """
-    #heatmap_kpr2_error_c()
-    #heatmap_kpr2_error_koff()
+
+    heatmap_kpr2_error_c()
+    heatmap_kpr2_error_koff()
