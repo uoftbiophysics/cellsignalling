d96b01c30a7912b35d86431dd619305d0453664f
mattsmart <mattsmart@hotmail.ca>
2020-05-13 20:23:15 -0400

    add option for determinant on right

--- figures/heatmaps_2ligands.py
+++ figures/heatmaps_2ligands.py
@@ -21,7 +21,7 @@ from settings import DIR_OUTPUT, DIR_INPUT, KON, KP, T, KF, ALPHA, C1, C2, KOFF,
 plt.style.use('parameters.mplstyle')  # particularIMporting
 
 # plot params
-FS = 20
+FS = 22
 SHOW = False
 
 # axes
@@ -51,8 +51,7 @@ class MidPointLogNorm(mpl.colors.LogNorm):
         return np.ma.masked_array(np.interp(np.log(value), x, y))
 
 
-def heatmap(ax, arr, xrange, yrange, xy_label, label, log_norm=True,
-                 xy_label_force=None, **kwargs):
+def heatmap(ax, arr, xrange, yrange, xy_label, label, log_norm=True, xy_label_force=None, **kwargs):
     """
     xrange: range of values for x
     yrange: range of y values
@@ -137,7 +136,7 @@ def heatmap(ax, arr, xrange, yrange, xy_label, label, log_norm=True,
 
     # TODO IDK why do ticks hide sometimes?
     CL = ax.contour(arr, levels=levels, linestyles=contour_linestyle, colors=contour_color, linewidths=contour_lindewidths)
-    ax.clabel(CL, CL.levels, inline=True, fmt=fmt)
+    ax.clabel(CL, CL.levels, inline=True, fmt=fmt, fontsize=FS-4)
 
     plt.tight_layout(h_pad=1)
 
@@ -167,36 +166,76 @@ def multiple_heatmaps(arrRelDetSigmaEst, arrRelErrorEst, array_x, array_y, fname
 
     #fig = plt.figure(figsize=(24,11));
     fig = plt.figure(figsize=(23,10));
-    gs = fig.add_gridspec(2,5, hspace=-0.01, wspace=-0.01, width_ratios=[1.,1.,1.,1.,0.1], height_ratios=[1.,1.])
     #gs = fig.add_gridspec(2,4, hspace=0.05, wspace=0.05, width_ratios=[1.,1.,1.,1.], height_ratios=[1.,1.])
 
     #ax0 = fig.add_subplot(gs[:,:-2])
-    ax0 = fig.add_subplot(gs[:,:-3])
-    ax1 = fig.add_subplot(gs[0,2])
-    ax2 = fig.add_subplot(gs[0,3])
-    ax3 = fig.add_subplot(gs[1,2])
-    ax4 = fig.add_subplot(gs[1,3])
-    ax5 = fig.add_subplot(gs[:,-1])
+    det_on_left = True
+    if det_on_left:
+        gs = fig.add_gridspec(2, 5, hspace=-0.01, wspace=-0.01, width_ratios=[1., 1., 1., 1., 0.1], height_ratios=[1., 1.])
 
-    # Determinant plot
-    #heatmap(ax0, arrDetSigmaEst[:,:], array_x, array_y, labels, r'Det($\Sigma_{est}$)', log_norm=True)
-    ax0, cbar0, im0 = heatmap(ax0, arrRelDetSigmaEst[:,:], array_x, array_y, labels, r'Det($\Sigma_{est}$)/(${c_1}^2{c_1}^2{k_{\mathrm{off},1}}^2{k_{\mathrm{off},2}}^2$)', log_norm=True)
-    cbar0.remove(); ax0.set_title(r'det($\Sigma_{est}$)/(${c_1}^2{c_2}^2{k_{\mathrm{off},1}}^2{k_{\mathrm{off},2}}^2$)', fontsize=FS)
+        ax0 = fig.add_subplot(gs[:,:-3])  # the det
+        ax1 = fig.add_subplot(gs[0,2])
+        ax2 = fig.add_subplot(gs[0,3])
+        ax3 = fig.add_subplot(gs[1,2])
+        ax4 = fig.add_subplot(gs[1,3])
+        ax5 = fig.add_subplot(gs[:,-1])   # the cbar
 
-    # each of the diagonals
-    ax1, cbar1, _ = heatmap(ax1, arrRelErrorEst[:,:,0,0], array_x, array_y, labels, r'$\langle \delta {c_1}^2 \rangle / {c_1}^2$', log_norm=log_select)
-    cbar1.remove(); ax1.tick_params(labelbottom=False); ax1.tick_params(labelleft=False); ax1.set_xticklabels([]); ax1.set_yticklabels([]); ax1.set_ylabel(''); ax1.xaxis.set_label_position('top'); ax1.set_xlabel(r'$\langle \delta {c_1}^2 \rangle / {c_1}^2$')
+        # Determinant plot
+        #heatmap(ax0, arrDetSigmaEst[:,:], array_x, array_y, labels, r'Det($\Sigma_{est}$)', log_norm=True)
+        ax0, cbar0, im0 = heatmap(ax0, arrRelDetSigmaEst[:,:], array_x, array_y, labels, r'Det($\Sigma_{est}$)/(${c_1}^2{c_1}^2{k_{\mathrm{off},1}}^2{k_{\mathrm{off},2}}^2$)', log_norm=True)
+        cbar0.remove(); ax0.set_title(r'det($\Sigma_{est}$)/(${c_1}^2{c_2}^2{k_{\mathrm{off},1}}^2{k_{\mathrm{off},2}}^2$)', fontsize=FS)
 
-    a2, cbar2, _ = heatmap(ax2, arrRelErrorEst[:,:,1,1], array_x, array_y, labels, r'$\langle \delta {k_{off}}^2 \rangle / {k_{off}}^2$', log_norm=log_select)
-    cbar2.remove(); ax2.tick_params(labelbottom=False); ax2.tick_params(labelleft=False); ax2.set_xticklabels([]); ax2.set_yticklabels([]); ax2.set_ylabel(''); ax2.xaxis.set_label_position('top'); ax2.set_xlabel(r'$\langle \delta {k_{\mathrm{off}}}^2 \rangle / {k_{\mathrm{off}}}^2$')
+        # each of the diagonals
+        ax1, cbar1, _ = heatmap(ax1, arrRelErrorEst[:,:,0,0], array_x, array_y, labels, r'$\langle \delta {c_1}^2 \rangle / {c_1}^2$', log_norm=log_select)
+        cbar1.remove(); ax1.tick_params(labelbottom=False); ax1.tick_params(labelleft=False); ax1.set_xticklabels([]); ax1.set_yticklabels([]); ax1.set_ylabel(''); ax1.xaxis.set_label_position('top'); ax1.set_xlabel(r'$\langle \delta {c_1}^2 \rangle / {c_1}^2$')
 
-    a3, cbar3, _ = heatmap(ax3, arrRelErrorEst[:,:,2,2], array_x, array_y, labels, r'$\langle \delta {c_2}^2 \rangle / {c_2}^2$', log_norm=log_select)
-    cbar3.remove(); ax3.tick_params(labelbottom=False); ax3.tick_params(labelleft=False); ax3.set_xticklabels([]); ax3.set_yticklabels([]); ax3.set_ylabel(''); ax3.xaxis.set_label_position('top'); ax3.set_xlabel( r'$\langle \delta {c_2}^2 \rangle / {c_2}^2$')
+        a2, cbar2, _ = heatmap(ax2, arrRelErrorEst[:,:,1,1], array_x, array_y, labels, r'$\langle \delta {k_{off}}^2 \rangle / {k_{off}}^2$', log_norm=log_select)
+        cbar2.remove(); ax2.tick_params(labelbottom=False); ax2.tick_params(labelleft=False); ax2.set_xticklabels([]); ax2.set_yticklabels([]); ax2.set_ylabel(''); ax2.xaxis.set_label_position('top'); ax2.set_xlabel(r'$\langle \delta {k_{\mathrm{off}}}^2 \rangle / {k_{\mathrm{off}}}^2$')
 
-    a4, cbar4, _ = heatmap( ax4, arrRelErrorEst[:,:,3,3], array_x, array_y, labels, r'$\langle \delta {k_{off,2}}^2 \rangle / {k_{off,2}}^2$', log_norm=log_select)
-    cbar4.remove(); ax4.tick_params(labelbottom=False); ax4.tick_params(labelleft=False); ax4.set_xticklabels([]); ax4.set_yticklabels([]); ax4.set_ylabel(''); ax4.xaxis.set_label_position('top'); ax4.set_xlabel( r'$\langle \delta {k_{\mathrm{off},2}}^2 \rangle / {k_{\mathrm{off},2}}^2$')
+        a3, cbar3, _ = heatmap(ax3, arrRelErrorEst[:,:,2,2], array_x, array_y, labels, r'$\langle \delta {c_2}^2 \rangle / {c_2}^2$', log_norm=log_select)
+        cbar3.remove(); ax3.tick_params(labelbottom=False); ax3.tick_params(labelleft=False); ax3.set_xticklabels([]); ax3.set_yticklabels([]); ax3.set_ylabel(''); ax3.xaxis.set_label_position('top'); ax3.set_xlabel( r'$\langle \delta {c_2}^2 \rangle / {c_2}^2$')
 
-    cb = fig.colorbar(im0, cax=ax5); cb.ax.tick_params(labelsize=FS)
+        a4, cbar4, _ = heatmap( ax4, arrRelErrorEst[:,:,3,3], array_x, array_y, labels, r'$\langle \delta {k_{off,2}}^2 \rangle / {k_{off,2}}^2$', log_norm=log_select)
+        cbar4.remove(); ax4.tick_params(labelbottom=False); ax4.tick_params(labelleft=False); ax4.set_xticklabels([]); ax4.set_yticklabels([]); ax4.set_ylabel(''); ax4.xaxis.set_label_position('top'); ax4.set_xlabel( r'$\langle \delta {k_{\mathrm{off},2}}^2 \rangle / {k_{\mathrm{off},2}}^2$')
+
+        cb = fig.colorbar(im0, cax=ax5); cb.ax.tick_params(labelsize=FS)
+
+
+    else:
+        gs = fig.add_gridspec(2, 6, hspace=-0.12, wspace=0.01, width_ratios=[1., 1., 0.2, 1., 1., 0.1], height_ratios=[1., 1.])
+
+        ax0 = fig.add_subplot(gs[:,3:5])  # the det
+        ax1 = fig.add_subplot(gs[0,0])
+        ax2 = fig.add_subplot(gs[0,1])
+        ax3 = fig.add_subplot(gs[1,0])
+        ax4 = fig.add_subplot(gs[1,1])
+        ax5 = fig.add_subplot(gs[:,-1])   # the cbar
+
+        # Determinant plot
+        #heatmap(ax0, arrDetSigmaEst[:,:], array_x, array_y, labels, r'Det($\Sigma_{est}$)', log_norm=True)
+        ax0, cbar0, im0 = heatmap(ax0, arrRelDetSigmaEst[:,:], array_x, array_y, labels, r'Det($\Sigma_{est}$)/(${c_1}^2{c_1}^2{k_{\mathrm{off},1}}^2{k_{\mathrm{off},2}}^2$)', log_norm=True)
+        cbar0.remove(); ax0.set_title(r'det($\Sigma_{est}$)/(${c_1}^2{c_2}^2{k_{\mathrm{off},1}}^2{k_{\mathrm{off},2}}^2$)', fontsize=FS)
+
+        # each of the diagonals
+        ax1, cbar1, _ = heatmap(ax1, arrRelErrorEst[:,:,0,0], array_x, array_y, labels, r'$\langle \delta {c_1}^2 \rangle / {c_1}^2$', log_norm=log_select)
+        cbar1.remove(); ax1.tick_params(labelbottom=False); ax1.set_xticklabels([]);  ax1.set_xlabel('')
+        ax1.set_title(r'$\langle \delta {c_1}^2 \rangle / {c_1}^2$', fontsize=FS)
+
+        a2, cbar2, _ = heatmap(ax2, arrRelErrorEst[:,:,1,1], array_x, array_y, labels, r'$\langle \delta {k_{off}}^2 \rangle / {k_{off}}^2$', log_norm=log_select)
+        cbar2.remove(); ax2.tick_params(labelbottom=False); ax2.tick_params(labelleft=False); ax2.set_xticklabels([]); ax2.set_yticklabels([]); ax2.set_ylabel(''); ax2.set_xlabel('')
+        ax2.set_title(r'$\langle \delta {k_{\mathrm{off}}}^2 \rangle / {k_{\mathrm{off}}}^2$', fontsize=FS)
+
+
+        a3, cbar3, _ = heatmap(ax3, arrRelErrorEst[:,:,2,2], array_x, array_y, labels, r'$\langle \delta {c_2}^2 \rangle / {c_2}^2$', log_norm=log_select)
+        cbar3.remove();
+        ax3.set_title(r'$\langle \delta {c_2}^2 \rangle / {c_2}^2$', fontsize=FS)
+
+
+        a4, cbar4, _ = heatmap( ax4, arrRelErrorEst[:,:,3,3], array_x, array_y, labels, r'$\langle \delta {k_{off,2}}^2 \rangle / {k_{off,2}}^2$', log_norm=log_select)
+        cbar4.remove(); ax4.tick_params(labelleft=False); ax4.set_yticklabels([]); ax4.set_ylabel('')
+        ax4.set_title(r'$\langle \delta {k_{\mathrm{off},2}}^2 \rangle / {k_{\mathrm{off},2}}^2$', fontsize=FS)
+
+        cb = fig.colorbar(im0, cax=ax5, fraction=0.9); cb.ax.tick_params(labelsize=FS)
 
     # save
     #plt.tight_layout(h_pad=0.05, w_pad=0.05, rect=[0.,0.,1.,1.]) # need to change this to not have the title overlap, figuring it out still
