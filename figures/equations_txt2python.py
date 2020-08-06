c2279e41955e6884d68aa30155ed201f287c614e
dakirby <39995281+dakirby@users.noreply.github.com>
2020-08-06 09:43:40 -0400

    Fix equation_txt2python.py so that Sigmacrlb functions are only generated for model 2 (the only one we need for Figure S3)

--- figures/equations_txt2python.py
+++ figures/equations_txt2python.py
@@ -1,31 +1,32 @@
 import os
 
+
 def write_function(textfile, equationfile, path):
 
     filename = os.path.splitext(textfile)[0]
-    filename = filename.replace(path + os.path.sep,'')
+    filename = filename.replace(path + os.path.sep, '')
 
     with open(textfile, 'rU') as f:
         eqn = f.read().strip()
 
-    with open(equationfile,'a+') as g:
+    with open(equationfile, 'a+') as g:
         g.write("\n\ndef %(filename)s(c, koff, kon=KON, T=T, KF=KF, KP=KP):\n    return %(eqn)s" %{'filename' : filename, 'eqn': eqn} )
 
     return 0
 
+
 def write_function_2ligands(textfile, equationfile, path):
 
     filename = os.path.splitext(textfile)[0]
-    filename = filename.replace(path + os.path.sep,'')
+    filename = filename.replace(path + os.path.sep, '')
 
     with open(textfile, 'rU') as f:
         eqn = f.read().strip()
 
-    with open(equationfile,'a+') as g:
-        g.write("\n\ndef %(filename)s(c1, koff, c2, koff2, kon=KON, T=T, KP=KP, ALPHA=ALPHA):\n    return %(eqn)s" %{'filename' : filename, 'eqn': eqn} )
+    with open(equationfile, 'a+') as g:
+        g.write("\n\ndef %(filename)s(c1, koff, c2, koff2, kon=KON, T=T, KP=KP, ALPHA=ALPHA):\n    return %(eqn)s" % {'filename': filename, 'eqn': eqn})
 
     return 0
-
 
 
 if __name__ == '__main__':
@@ -38,13 +39,13 @@ if __name__ == '__main__':
         path = os.getcwd() + os.path.sep + 'input' + os.path.sep + 'mathematica_equations_2ligands'
         func_write = write_function_2ligands
 
-    with open(python_equation_file,'w+') as f:
+    with open(python_equation_file, 'w+') as f:
         f.write('import numpy as np\nimport os\nfrom heatmaps import KON, KP, T, KF, ALPHA\n\ndef Sqrt(x):\n    return np.sqrt(x)')
 
     for r, d, f in os.walk(path):
         for file in f:
             if '.txt' in file:
-                    func_write(path + os.path.sep + file, python_equation_file, path)
+                func_write(path + os.path.sep + file, python_equation_file, path)
 
     # extra functions to write for the 1 ligand case
     if python_equation_file == 'equations.py':
@@ -52,16 +53,15 @@ if __name__ == '__main__':
         for r, d, f in os.walk(path):
             for file in f:
                 if 'DetSigmacrlb' in file:
-                    with open(python_equation_file,'a+') as g:
+                    with open(python_equation_file, 'a+') as g:
                         filename = os.path.splitext(file)[0]
                         g.write("\n\ndef Rel%(filename)s(c, koff, kon=KON, T=T, KF=KF, KP=KP):\n    return ( %(filename)s(c, koff, kon, T, KF, KP) )/( c**2 * koff**2 )" %{'filename' : filename} )
 
-
         # trace equation and eigenvalue equations
-        with open(python_equation_file,'a+') as g:
+        with open(python_equation_file, 'a+') as g:
 
             # dedim relative errors
-            for model in ['2', '3', '4', '2NoTrace','3NoTrace','4NoTrace']:
+            for model in ['2', '2NoTrace']:
                 # trace
                 g.write("\n\ndef traceSigmacrlb%(name)s(c, koff, kon=KON, T=T, KF=KF, KP=KP):\n    return (SigmacrlbC%(name)s(c, koff, kon, T, KF, KP)+SigmacrlbK%(name)s(c, koff, kon, T, KF, KP))" %{'name' : model})
                 # high and low eigenvalue
@@ -81,6 +81,6 @@ if __name__ == '__main__':
     if python_equation_file == 'equations2ligands.py':
 
         # matrix equations
-        with open(python_equation_file,'a+') as g:
-            for matrix in ["__dmudthetaInv__","sigmadata"]:
-                g.write("\n\ndef matrix_%(matrix)s(c1, koff, c2, koff2, kon=KON, T=T, KP=KP,  ALPHA=ALPHA):\n    return np.array([[%(matrix)s11(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s12(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s13(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s14(c1, koff, c2, koff2, kon, T, KP, ALPHA)],[%(matrix)s21(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s22(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s23(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s24(c1, koff, c2, koff2, kon, T, KP, ALPHA)],[%(matrix)s31(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s32(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s33(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s34(c1, koff, c2, koff2, kon, T, KP, ALPHA)],[%(matrix)s41(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s42(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s43(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s44(c1, koff, c2, koff2, kon, T, KP, ALPHA)]])" %{'matrix' : matrix})
+        with open(python_equation_file, 'a+') as g:
+            for matrix in ["__dmudthetaInv__", "sigmadata"]:
+                g.write("\n\ndef matrix_%(matrix)s(c1, koff, c2, koff2, kon=KON, T=T, KP=KP,  ALPHA=ALPHA):\n    return np.array([[%(matrix)s11(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s12(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s13(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s14(c1, koff, c2, koff2, kon, T, KP, ALPHA)],[%(matrix)s21(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s22(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s23(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s24(c1, koff, c2, koff2, kon, T, KP, ALPHA)],[%(matrix)s31(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s32(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s33(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s34(c1, koff, c2, koff2, kon, T, KP, ALPHA)],[%(matrix)s41(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s42(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s43(c1, koff, c2, koff2, kon, T, KP, ALPHA), %(matrix)s44(c1, koff, c2, koff2, kon, T, KP, ALPHA)]])" % {'matrix': matrix})
