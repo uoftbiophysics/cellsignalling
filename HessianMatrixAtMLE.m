dfb45d773c32a0d71b3a06ece1bbef2b2c02fb5d
mattsmart <mattsmart@hotmail.ca>
2020-08-11 11:18:02 -0400

    upload mathematica files as checkpoint for README

---
+++ HessianMatrixAtMLE.m
@@ -0,0 +1,284 @@
+{{(-2*E^((koff + c*kon)*t)*(-1 + E^((koff + c*kon)*t))^5*koff^3*kp*
+     (kp*m - koff*n)^2 - (c*E^(2*(koff + c*kon)*t)*koff*kon*
+      (koff*m*n*(m + n) + c*kon*m*n*(m + n) + koff^3*t^2*(n + kp*t) + 
+       koff^2*t*(m*n + n^2 + kp*n*t + kp^2*t^2))*
+      (E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+           (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+          4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+             (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+        E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+        c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+          c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+          m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^2)/kp + 
+    ((E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+           (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+          4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+             (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+        E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+        c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+          c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+          m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^2*
+      (koff^4*(2*(-2 + m)*n + 6*E^(2*(koff + c*kon)*t)*m*(-1 + m + 2*n) + 
+         E^((koff + c*kon)*t)*(-6*m^2 + m^3 - m*(-6 + n) + 4*n)) + 
+       koff^3*((-1 + E^((koff + c*kon)*t))^2*kp + 
+         2*c*kon*m*(E^((koff + c*kon)*t)*(-3 - 10*m + 3*m^2 - 2*n) + 4*n + 
+           E^(2*(koff + c*kon)*t)*(3 + 10*n))) - c*koff^2*kon*
+        (10*(-1 + E^((koff + c*kon)*t))^2*kp + 
+         c*kon*(E^(2*(koff + c*kon)*t)*m*(-24 + 15*m - 5*n) - 12*(1 + m)*n + 
+           E^((koff + c*kon)*t)*(20*m^2 - 15*m^3 + 12*n + 6*m*(4 + n)))) - 
+       2*c^2*koff*kon^2*(-5*(-1 + E^((koff + c*kon)*t))^2*kp + 
+         c*kon*(-4*(1 + m)*n + 3*E^(2*(koff + c*kon)*t)*m*(-1 + m + n) + 
+           E^((koff + c*kon)*t)*(-10*m^3 + 4*n + m*(3 + 2*n)))) - 
+       E^(2*(koff + c*kon)*t)*koff^6*t^2 - E^(2*(koff + c*kon)*t)*koff^5*t*
+        (-4*m - 4*n + kp*t) + c^4*kon^4*m*(2*n + E^(2*(koff + c*kon)*t)*
+          (-6 + 3*m - 3*n + 4*c*kon*t) + E^((koff + c*kon)*t)*
+          (6 + 10*m^2 - n + 4*c*kon*t + c^2*kon^2*t^2 + m*(6 + 4*c*kon*t)))))/
+     (koff + c*kon)^2 + (2*koff^2*(E^((koff + c*kon)*t)*koff^2*m*
+        (-2 + E^((koff + c*kon)*t)*(2 + 4*m + 3*n)) - 
+       koff*((-1 + E^((koff + c*kon)*t))^2*kp - 4*c*E^((koff + c*kon)*t)*kon*
+          m*(-1 + E^((koff + c*kon)*t)*(1 + 2*m + n))) + 
+       E^(2*(koff + c*kon)*t)*koff^4*t^2 + E^(2*(koff + c*kon)*t)*koff^3*t*
+        (2*m + 2*n + kp*t) + c^2*E^((koff + c*kon)*t)*kon^2*
+        (4*E^((koff + c*kon)*t)*m^2 + c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+         m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))*
+      (koff^2*(E^(2*(koff + c*kon)*t)*m*(3 + 2*m) + E^((koff + c*kon)*t)*
+           (-3*m + m^2 - n) + n) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+          2*c*kon*(E^(2*(koff + c*kon)*t)*m*(2 + 3*m - n) + 
+            E^((koff + c*kon)*t)*(-2*m + 2*m^2 - n) + n)) + 
+        E^(2*(koff + c*kon)*t)*koff^4*t^2 + E^(2*(koff + c*kon)*t)*koff^3*t*
+         (m + n + kp*t) + c*kon*(2*(-1 + E^((koff + c*kon)*t))^2*kp + 
+          c*kon*(n + E^((koff + c*kon)*t)*(3*m^2 - n + c*kon*t*(2 + 
+                c*kon*t) + m*(-1 + 2*c*kon*t)) + E^(2*(koff + c*kon)*t)*
+             (4*m^2 + c*kon*t*(-2 - n + 2*c*kon*t) + m*(1 - 2*n + 
+                3*c*kon*t)))))^2)/(koff + c*kon)^2)/
+   (c^2*(E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+         (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+        4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+           (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+      E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+      c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+        c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+        m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^3), 
+  ((-2*E^((koff + c*kon)*t)*(-1 + E^((koff + c*kon)*t))^5*koff^2*kp*
+      (kp*m - koff*n)^2)/c - 4*E^((koff + c*kon)*t)*
+     (-1 + E^((koff + c*kon)*t))^5*koff*kon*kp*(kp*m - koff*n)^2 - 
+    (E^((koff + c*kon)*t)*n*(E^((koff + c*kon)*t)*(4*c^2*koff*kon^2*m^2 + 
+         c^3*kon^3*m^2 + c*koff^2*kon*(6*m^2 - n) + koff^4*m*t + 
+         koff^3*(3*m^2 - n - kp*t)) + koff^2*(c*kon*n + koff*(n + kp*t)))*
+      (E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+           (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+          4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+             (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+        E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+        c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+          c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+          m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^2)/
+     (c*koff*kp) + 
+    ((-(c*koff*kon*(2*(-1 + E^((koff + c*kon)*t))^2*kp + 
+          c*E^((koff + c*kon)*t)*kon*m*(2 - 8*m + E^((koff + c*kon)*t)*
+             (-2 + 3*m - 6*n)))) + koff^2*(4*(-1 + E^((koff + c*kon)*t))^2*
+          kp + c*kon*(2*E^((koff + c*kon)*t)*(4*m + 6*m^2 - n) + 2*n + 
+           E^(2*(koff + c*kon)*t)*m*(-8 + 3*m + 4*n))) - 
+       E^(2*(koff + c*kon)*t)*koff^5*t^2 - E^((koff + c*kon)*t)*koff^4*t*
+        (-((2 + E^((koff + c*kon)*t))*m) + 2*E^((koff + c*kon)*t)*
+          (n + kp*t)) + koff^3*(3*E^(2*(koff + c*kon)*t)*(-2 + m)*m + 
+         2*(n + kp*t) - 2*E^((koff + c*kon)*t)*(-3*m - 3*m^2 + n + kp*t)) + 
+       c^3*E^((koff + c*kon)*t)*kon^3*((2 - 3*E^((koff + c*kon)*t))*m^2 + 
+         c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+         m*(-4 + E^((koff + c*kon)*t)*(4 + 2*n - c*kon*t))))*
+      (E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+          (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+         4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+            (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+       E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+       c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+         c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+         m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))*
+      (koff^2*(E^(2*(koff + c*kon)*t)*m*(3 + 2*m) + E^((koff + c*kon)*t)*
+          (-3*m + m^2 - n) + n) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+         2*c*kon*(E^(2*(koff + c*kon)*t)*m*(2 + 3*m - n) + 
+           E^((koff + c*kon)*t)*(-2*m + 2*m^2 - n) + n)) + 
+       E^(2*(koff + c*kon)*t)*koff^4*t^2 + E^(2*(koff + c*kon)*t)*koff^3*t*
+        (m + n + kp*t) + c*kon*(2*(-1 + E^((koff + c*kon)*t))^2*kp + 
+         c*kon*(n + E^((koff + c*kon)*t)*(3*m^2 - n + c*kon*t*(2 + c*kon*t) + 
+             m*(-1 + 2*c*kon*t)) + E^(2*(koff + c*kon)*t)*
+            (4*m^2 + c*kon*t*(-2 - n + 2*c*kon*t) + m*(1 - 2*n + 3*c*kon*
+                t))))))/(c*(koff + c*kon)^2) + 
+    ((E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+           (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+          4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+             (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+        E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+        c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+          c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+          m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^2*
+      (c*koff*kon*(13*(-1 + E^((koff + c*kon)*t))^2*kp + 
+         c*kon*(E^(2*(koff + c*kon)*t)*m*(-21 + 15*m - 10*n) + 
+           4*(1 + 2*m)*n + E^((koff + c*kon)*t)*(21*m + 10*m^2 + 20*m^3 - 
+             4*n - 4*m*n))) + koff^2*(-4*(-1 + E^((koff + c*kon)*t))^2*kp - 
+         c*kon*(-12*(1 + m)*n + E^(2*(koff + c*kon)*t)*m*(-1 + 20*n) + 
+           E^((koff + c*kon)*t)*(m - 15*m^3 + 12*n + 6*m*n))) + 
+       E^(2*(koff + c*kon)*t)*koff^5*t^2 - 
+       koff^3*(-6*(1 + m)*n + E^(2*(koff + c*kon)*t)*m*(-9 + 6*m + 10*n) + 
+         2*kp*t + E^((koff + c*kon)*t)*(4*m^2 - 5*m^3 + 6*n + 3*m*(3 + n) - 
+           2*kp*t)) + koff^4*t*(E^((koff + c*kon)*t)*(-3*m + m^2 - n) + 2*n - 
+         2*E^(2*(koff + c*kon)*t)*(2*m + n - kp*t)) + 
+       c^2*kon^2*(-4*(-1 + E^((koff + c*kon)*t))^2*kp + 
+         c*kon*(2*(-1 + m)*n + E^(2*(koff + c*kon)*t)*
+            (9*m^2 + 2*c*kon*t*(2 + n - 2*c*kon*t) + m*(-13 + c*kon*t)) + 
+           E^((koff + c*kon)*t)*(10*m^3 + m^2*(6 + 4*c*kon*t) + 
+             m*(13 - n + c*kon*t + c^2*kon^2*t^2) + 
+             2*(n - c*kon*t*(2 + c*kon*t)))))))/(c*(koff + c*kon)^2))/
+   (E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+        (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+       4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+          (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+     E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+     c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+       c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+       m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^3}, 
+ {((-2*E^((koff + c*kon)*t)*(-1 + E^((koff + c*kon)*t))^5*koff^2*kp*
+      (kp*m - koff*n)^2)/c - 4*E^((koff + c*kon)*t)*
+     (-1 + E^((koff + c*kon)*t))^5*koff*kon*kp*(kp*m - koff*n)^2 - 
+    (E^((koff + c*kon)*t)*n*(E^((koff + c*kon)*t)*(4*c^2*koff*kon^2*m^2 + 
+         c^3*kon^3*m^2 + c*koff^2*kon*(6*m^2 - n) + koff^4*m*t + 
+         koff^3*(3*m^2 - n - kp*t)) + koff^2*(c*kon*n + koff*(n + kp*t)))*
+      (E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+           (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+          4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+             (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+        E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+        c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+          c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+          m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^2)/
+     (c*koff*kp) + 
+    ((-(c*koff*kon*(2*(-1 + E^((koff + c*kon)*t))^2*kp + 
+          c*E^((koff + c*kon)*t)*kon*m*(2 - 8*m + E^((koff + c*kon)*t)*
+             (-2 + 3*m - 6*n)))) + koff^2*(4*(-1 + E^((koff + c*kon)*t))^2*
+          kp + c*kon*(2*E^((koff + c*kon)*t)*(4*m + 6*m^2 - n) + 2*n + 
+           E^(2*(koff + c*kon)*t)*m*(-8 + 3*m + 4*n))) - 
+       E^(2*(koff + c*kon)*t)*koff^5*t^2 - E^((koff + c*kon)*t)*koff^4*t*
+        (-((2 + E^((koff + c*kon)*t))*m) + 2*E^((koff + c*kon)*t)*
+          (n + kp*t)) + koff^3*(3*E^(2*(koff + c*kon)*t)*(-2 + m)*m + 
+         2*(n + kp*t) - 2*E^((koff + c*kon)*t)*(-3*m - 3*m^2 + n + kp*t)) + 
+       c^3*E^((koff + c*kon)*t)*kon^3*((2 - 3*E^((koff + c*kon)*t))*m^2 + 
+         c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+         m*(-4 + E^((koff + c*kon)*t)*(4 + 2*n - c*kon*t))))*
+      (E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+          (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+         4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+            (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+       E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+       c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+         c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+         m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))*
+      (koff^2*(E^(2*(koff + c*kon)*t)*m*(3 + 2*m) + E^((koff + c*kon)*t)*
+          (-3*m + m^2 - n) + n) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+         2*c*kon*(E^(2*(koff + c*kon)*t)*m*(2 + 3*m - n) + 
+           E^((koff + c*kon)*t)*(-2*m + 2*m^2 - n) + n)) + 
+       E^(2*(koff + c*kon)*t)*koff^4*t^2 + E^(2*(koff + c*kon)*t)*koff^3*t*
+        (m + n + kp*t) + c*kon*(2*(-1 + E^((koff + c*kon)*t))^2*kp + 
+         c*kon*(n + E^((koff + c*kon)*t)*(3*m^2 - n + c*kon*t*(2 + c*kon*t) + 
+             m*(-1 + 2*c*kon*t)) + E^(2*(koff + c*kon)*t)*
+            (4*m^2 + c*kon*t*(-2 - n + 2*c*kon*t) + m*(1 - 2*n + 3*c*kon*
+                t))))))/(c*(koff + c*kon)^2) + 
+    ((E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+           (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+          4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+             (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+        E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+        c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+          c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+          m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^2*
+      (c*koff*kon*(13*(-1 + E^((koff + c*kon)*t))^2*kp + 
+         c*kon*(E^(2*(koff + c*kon)*t)*m*(-21 + 15*m - 10*n) + 
+           4*(1 + 2*m)*n + E^((koff + c*kon)*t)*(21*m + 10*m^2 + 20*m^3 - 
+             4*n - 4*m*n))) + koff^2*(-4*(-1 + E^((koff + c*kon)*t))^2*kp - 
+         c*kon*(-12*(1 + m)*n + E^(2*(koff + c*kon)*t)*m*(-1 + 20*n) + 
+           E^((koff + c*kon)*t)*(m - 15*m^3 + 12*n + 6*m*n))) + 
+       E^(2*(koff + c*kon)*t)*koff^5*t^2 - 
+       koff^3*(-6*(1 + m)*n + E^(2*(koff + c*kon)*t)*m*(-9 + 6*m + 10*n) + 
+         2*kp*t + E^((koff + c*kon)*t)*(4*m^2 - 5*m^3 + 6*n + 3*m*(3 + n) - 
+           2*kp*t)) + koff^4*t*(E^((koff + c*kon)*t)*(-3*m + m^2 - n) + 2*n - 
+         2*E^(2*(koff + c*kon)*t)*(2*m + n - kp*t)) + 
+       c^2*kon^2*(-4*(-1 + E^((koff + c*kon)*t))^2*kp + 
+         c*kon*(2*(-1 + m)*n + E^(2*(koff + c*kon)*t)*
+            (9*m^2 + 2*c*kon*t*(2 + n - 2*c*kon*t) + m*(-13 + c*kon*t)) + 
+           E^((koff + c*kon)*t)*(10*m^3 + m^2*(6 + 4*c*kon*t) + 
+             m*(13 - n + c*kon*t + c^2*kon^2*t^2) + 
+             2*(n - c*kon*t*(2 + c*kon*t)))))))/(c*(koff + c*kon)^2))/
+   (E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+        (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+       4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+          (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+     E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+     c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+       c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+       m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^3, 
+  (8*E^((koff + c*kon)*t)*(-1 + E^((koff + c*kon)*t))^5*koff^4*kp*
+     (kp*m - koff*n)^2 + (12*E^((koff + c*kon)*t)*(-1 + E^((koff + c*kon)*t))^
+       5*koff^5*kp*(kp*m - koff*n)^2)/(c*kon) + 
+    (koff*(c*koff*kon*(2*(-1 + E^((koff + c*kon)*t))^2*kp + 
+          c*E^((koff + c*kon)*t)*kon*m*(2 - 8*m + E^((koff + c*kon)*t)*
+             (-2 + 3*m - 6*n))) - koff^2*(4*(-1 + E^((koff + c*kon)*t))^2*
+           kp + c*kon*(2*E^((koff + c*kon)*t)*(4*m + 6*m^2 - n) + 2*n + 
+            E^(2*(koff + c*kon)*t)*m*(-8 + 3*m + 4*n))) + 
+        E^(2*(koff + c*kon)*t)*koff^5*t^2 + E^((koff + c*kon)*t)*koff^4*t*
+         (-((2 + E^((koff + c*kon)*t))*m) + 2*E^((koff + c*kon)*t)*
+           (n + kp*t)) - koff^3*(3*E^(2*(koff + c*kon)*t)*(-2 + m)*m + 
+          2*(n + kp*t) - 2*E^((koff + c*kon)*t)*(-3*m - 3*m^2 + n + kp*t)) - 
+        c^3*E^((koff + c*kon)*t)*kon^3*((2 - 3*E^((koff + c*kon)*t))*m^2 + 
+          c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+          m*(-4 + E^((koff + c*kon)*t)*(4 + 2*n - c*kon*t))))^2*
+      (E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+          (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+         4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+            (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+       E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+       c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+         c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+         m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t)))))/
+     (koff + c*kon)^2 - (2*E^(2*(koff + c*kon)*t)*n*(4*c^4*koff*kon^4*m^2 + 
+       c^5*kon^5*m^2 + 4*c^2*koff^3*kon^2*m*(m + n) + 
+       c^3*koff^2*kon^3*m*(6*m + n) + c*koff^4*kon*m*(m + 6*n) + 
+       koff^5*n*(3*m + koff*t))*(E^((koff + c*kon)*t)*koff^2*m*
+         (-2 + E^((koff + c*kon)*t)*(2 + 4*m + 3*n)) - 
+        koff*((-1 + E^((koff + c*kon)*t))^2*kp - 4*c*E^((koff + c*kon)*t)*kon*
+           m*(-1 + E^((koff + c*kon)*t)*(1 + 2*m + n))) + 
+        E^(2*(koff + c*kon)*t)*koff^4*t^2 + E^(2*(koff + c*kon)*t)*koff^3*t*
+         (2*m + 2*n + kp*t) + c^2*E^((koff + c*kon)*t)*kon^2*
+         (4*E^((koff + c*kon)*t)*m^2 + c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+          m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^2)/(c*kon*kp) + 
+    (2*koff^2*(E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+           (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+          4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+             (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+        E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+        c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+          c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+          m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^2*
+      (2*koff^2*(5*(-1 + E^((koff + c*kon)*t))^2*kp + 
+         2*c*kon*m*(E^((koff + c*kon)*t)*(2 + 5*m + 5*m^2 - n) + 2*n + 
+           E^(2*(koff + c*kon)*t)*(-2 + 5*n))) - 
+       c*koff*kon*(10*(-1 + E^((koff + c*kon)*t))^2*kp + 
+         c*kon*(3*E^(2*(koff + c*kon)*t)*m*(-6 + 5*m - 5*n) - 2*(-2 + m)*n + 
+           E^((koff + c*kon)*t)*(-15*m^3 - 4*n + m*(18 + n)))) - 
+       E^((koff + c*kon)*t)*koff^5*(E^((koff + c*kon)*t) - m)*t^2 - 
+       koff^4*t*(-2*(2*n + kp*t) + E^((koff + c*kon)*t)*(-6*m - 4*m^2 + 2*n + 
+           kp*t) + E^(2*(koff + c*kon)*t)*(-4*m + 3*kp*t)) + 
+       koff^3*((4 + 6*m)*n + 2*E^(2*(koff + c*kon)*t)*m*(-6 + 3*m + 4*n) + 
+         8*kp*t + E^((koff + c*kon)*t)*(14*m^2 + 10*m^3 - 3*m*(-4 + n) - 
+           4*(n + 2*kp*t))) + c^2*kon^2*((-1 + E^((koff + c*kon)*t))^2*kp + 
+         c*E^((koff + c*kon)*t)*kon*(5*m^3 + 
+           m^2*(-6 - 9*E^((koff + c*kon)*t) + c*kon*t) - 
+           m*(14 + 4*c*kon*t + E^((koff + c*kon)*t)*(-14 - 3*n + c*kon*t)) + 
+           c*kon*t*(2 + E^((koff + c*kon)*t)*(-2 - n + 4*c*kon*t))))))/
+     (koff + c*kon)^2)/(2*koff^3*
+    (E^((koff + c*kon)*t)*koff^2*m*(-2 + E^((koff + c*kon)*t)*
+         (2 + 4*m + 3*n)) - koff*((-1 + E^((koff + c*kon)*t))^2*kp - 
+        4*c*E^((koff + c*kon)*t)*kon*m*(-1 + E^((koff + c*kon)*t)*
+           (1 + 2*m + n))) + E^(2*(koff + c*kon)*t)*koff^4*t^2 + 
+      E^(2*(koff + c*kon)*t)*koff^3*t*(2*m + 2*n + kp*t) + 
+      c^2*E^((koff + c*kon)*t)*kon^2*(4*E^((koff + c*kon)*t)*m^2 + 
+        c^2*E^((koff + c*kon)*t)*kon^2*t^2 + 
+        m*(-2 + E^((koff + c*kon)*t)*(2 + n + 2*c*kon*t))))^3)}}
